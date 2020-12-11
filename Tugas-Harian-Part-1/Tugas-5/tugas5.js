00a622d4e137903b08240c6ffb9d88a6324a7a5b
egabagus <egabagus1@gmail.com>
2020-12-11 10:45:27 +0700

    mengerjakan tugas5

---
+++ Tugas-Harian-Part-1/Tugas-5/tugas5.js
@@ -0,0 +1,74 @@
+// Soal no.1
+function halo(){ // Jawaban Soal no.1
+    return "Halo Sanbers!"; // Jawaban Soal no.1
+}
+
+console.log("\n" + halo()) // Jawaban Soal no.1
+
+
+// Soal no.2
+var angkaPertama = 12;
+var angkaKedua = 4;
+
+function kalikan() {  // Jawaban Soal no.2
+    return angkaPertama * angkaKedua  // Jawaban Soal no.2
+  }
+console.log(angkaPertama + " * " + angkaKedua + " = " + kalikan())  // Jawaban Soal no.2
+
+
+// Soal no.3
+var nama = "John Doe";
+var umur = 30;
+var alamat = "Belum punya";
+var hobi = "gaming";
+var perkenalan = introduce(nama, umur, alamat, hobi);
+
+function introduce(){  // Jawaban Soal no.3
+    return "nama saya " + nama + ", umur saya " + umur + ", alamat saya ada di " + alamat + ", dan saya mempunyai hobi " + hobi;  // Jawaban Soal no.3
+}
+console.log(perkenalan)  // Jawaban Soal no.3
+
+
+// Soal no.4
+var arrayDaftarPeserta = ["Asep", "laki-laki", "baca buku" , 1992];
+var objectDaftarPeserta = {  // Jawaban Soal no.4
+    nama : "Asep",
+    jenis_kelamin : "Laki-laki",  // Jawaban Soal no.4
+    hobby : "Baca Buku",
+    tahun_lahir : 1992  // Jawaban Soal no.4
+}
+
+console.log(objectDaftarPeserta)  // Jawaban Soal no.4
+
+
+// Soal no.5
+var objDataBuah = [{
+    no : 1,
+    name : "Stroberi",
+    warna : "Merah",
+    biji : "Ada",
+    harga : 9000
+},{
+    no : 2,
+    name : "Jeruk",
+    warna : "Orange",
+    biji : "Ada",
+    harga : 8000
+},{
+    no : 3,
+    name : "Semangka",
+    warna : "Hijau & Merah",
+    biji : "Ada",
+    harga : 10000
+},{
+    no : 4,
+    name : "Pisang",
+    warna : "Kuning",
+    biji : "Tidak Ada",
+    harga : 5000
+}]
+var objDataFilter = objDataBuah.filter(function(item){
+    return item.no == 1;
+})
+
+console.log(objDataFilter)
