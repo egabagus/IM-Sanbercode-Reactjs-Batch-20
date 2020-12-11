be2da49a6fdb63c7a5b710a6d3d245e83c4f661b
egabagus <egabagus1@gmail.com>
2020-12-11 07:41:31 +0700

    revisi tugas 4

--- Tugas-Harian-Part-1/Tugas-4/tugas4.js
+++ Tugas-Harian-Part-1/Tugas-4/tugas4.js
@@ -36,20 +36,20 @@ for (;angka3 < 20; angka3++){
 
 
 // Soal no.3
-for (var i=1;i<=5;i++){ // Jawaban Soal no.3
-    for (var j=1;j<=i;j++){ // Jawaban Soal no.3
-        console.log("*"); // Jawaban Soal no.3
+
+var angka4 = "";
+for (var i=0;i < 8; i++){ // Jawaban Soal no.3
+    for (var j=1; j<i; j++){ // Jawaban Soal no.3
+        angka4 += "#" // Jawaban Soal no.3
     }
+    angka4 += "\n" + "\n"
 }
+console.log(angka4);
 
 // Soal no.4
 var kalimat = "saya sangat senang belajar javascript";
-var saya = kalimat.slice(0,4);
-var sangat = kalimat.slice(5,11);
-var senang = kalimat.slice(12,18);
-var belajar = kalimat.slice(19,26);
-var javascript = kalimat.slice(27,38);
-console.log(saya + " , " + sangat + " , " + senang + " , " + belajar + " , " + javascript);
+var ubah = kalimat.split(" ");
+console.log(ubah);
 
 
 // Soal no.5
